bb19c511e1015493bc3c70be0ce971c23f9abeab
lishuaishuai <lishuaishuai.it@gmail.com>
2019-07-17 09:50:33 +0800

    feat: 增加example 更新tc-tools

---
+++ examples/button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../src'
+
+import '../assets/button.less'
+
+class Test extends React.Component {
+  render() {
+    return <Button>12345</Button>
+  }
+}
+
+export default Test
